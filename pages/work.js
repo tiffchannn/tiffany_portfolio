7dc243bab73d67d95c8891c2b7fd95b4dbe93cbb
tiffchannn <tiffany.yi.chan@gmail.com>
2021-04-13 10:31:10 -0700

    Added and removed reactstrap

--- pages/work.js
+++ pages/work.js
@@ -21,9 +21,9 @@ export default function Work() {
         {/* <div className={styles.nav_links}> */}
         <div className="text-white flex justify-center mt-7 lg:mt-7 lg:mr-6 font-default italic text-xl">
 
-          <Link href="/"><a className="text-white mr-2 lg:mr-9 hover:text-nude-500">Home</a></Link>
-          <Link href="/about"><a className="text-white mr-2 lg:mr-9 hover:text-nude-500">About</a></Link>
-          <Link href="/contact"><a className="text-white mr-4 lg:mr-12 hover:text-nude-500">Contact</a></Link>
+          <Link href="/"><a className="mr-2 lg:mr-9 hover:text-nude-500">Home</a></Link>
+          <Link href="/about"><a className="mr-2 lg:mr-9 hover:text-nude-500">About</a></Link>
+          <Link href="/contact"><a className="mr-4 lg:mr-12 hover:text-nude-500">Contact</a></Link>
         </div>
       </div>
 
